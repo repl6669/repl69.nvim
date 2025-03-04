9fe159c654ee50a281a7f8bab8e2893052d0b353
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-03-04 11:28:42 +0100

    add repl69 lualine theme

---
+++ lua/lualine/themes/repl69.lua
@@ -0,0 +1,40 @@
+local palette = require("repl69.palette")
+local config = require("repl69.config")
+
+local bg_base = palette.black
+if config.options.styles.transparency then
+	bg_base = "NONE"
+end
+
+return {
+	normal = {
+		a = { bg = palette.gray19, fg = palette.black, gui = "bold" },
+		b = { bg = palette.gray06, fg = palette.gray16 },
+		c = { bg = bg_base, fg = palette.gray21 },
+	},
+	insert = {
+		a = { bg = palette.green, fg = palette.black, gui = "bold" },
+		b = { bg = palette.gray06, fg = palette.gray16 },
+		c = { bg = bg_base, fg = palette.gray21 },
+	},
+	visual = {
+		a = { bg = palette.blue, fg = palette.black, gui = "bold" },
+		b = { bg = palette.gray06, fg = palette.gray16 },
+		c = { bg = bg_base, fg = palette.gray21 },
+	},
+	replace = {
+		a = { bg = palette.blue, fg = palette.black, gui = "bold" },
+		b = { bg = palette.gray06, fg = palette.gray16 },
+		c = { bg = bg_base, fg = palette.gray21 },
+	},
+	command = {
+		a = { bg = palette.cyan, fg = palette.black, gui = "bold" },
+		b = { bg = palette.gray06, fg = palette.gray16 },
+		c = { bg = bg_base, fg = palette.gray21 },
+	},
+	inactive = {
+		a = { bg = bg_base, fg = palette.gray04, gui = "bold" },
+		b = { bg = bg_base, fg = palette.gray04 },
+		c = { bg = bg_base, fg = palette.gray04 },
+	},
+}
