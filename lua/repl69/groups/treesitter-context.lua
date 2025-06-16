2e418603292c6c4a8e0afc188402e6435db54c00
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-06-16 22:05:44 +0200

    feat: update treesitter context hl

--- lua/repl69/groups/treesitter-context.lua
+++ lua/repl69/groups/treesitter-context.lua
@@ -8,8 +8,9 @@ M.url = "https://github.com/nvim-treesitter/nvim-treesitter-context"
 function M.get(c)
   -- stylua: ignore
   return {
-    TreesitterContext = { bg = Util.blend_bg(c.gray800, 0.8) },
+    TreesitterContext = { bg = Util.blend_bg(c.gray950, 0.8) },
   }
 end
 
 return M
+
