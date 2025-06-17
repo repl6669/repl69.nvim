d669b1c98edce699ae410cb77c099f83303eaf9c
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-06-17 06:32:52 +0200

    feat: update render-markdown colors

--- lua/repl69/groups/render-markdown.lua
+++ lua/repl69/groups/render-markdown.lua
@@ -9,16 +9,26 @@ function M.get(c, opts)
   -- stylua: ignore
   local ret = {
     RenderMarkdownBullet    = {fg = c.orange}, -- horizontal rule
-    RenderMarkdownCode      = { bg = c.bg_dark },
+    RenderMarkdownCode      = { bg = c.bg_darker},
     RenderMarkdownDash      = {fg = c.orange}, -- horizontal rule
-    RenderMarkdownTableHead = { fg = c.red},
-    RenderMarkdownTableRow  = { fg = c.orange},
+    RenderMarkdownTableHead = { fg = c.orange},
+    RenderMarkdownTableRow  = { fg = c.orange700},
     RenderMarkdownCodeInline = "@markup.raw.markdown_inline"
   }
-  
-  local rainbow_colors = { c.red, c.orange, c.yellow, c.green, c.cyan, c.blue, c.purple, c.pink }
+
+  local rainbow_colors = {
+    c.green500,
+    c.orange500,
+    c.yellow500,
+    c.red500,
+    c.pink500,
+    c.purple500,
+    c.blue500,
+    c.cyan500,
+  }
+
   for i, color in ipairs(rainbow_colors) do
-    ret["RenderMarkdownH" .. i .. "Bg"] = { bg = Util.blend_bg(color, 0.1) }
+    ret["RenderMarkdownH" .. i .. "Bg"] = { bg = Util.blend_bg(color, 0.2) }
     ret["RenderMarkdownH" .. i .. "Fg"] = { fg = color, bold = true }
   end
   return ret
